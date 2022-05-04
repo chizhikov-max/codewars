c333b2f424d465e8840870fb98f777ee6ce0ea9d
Максим Чижиков <chizhikov.max@hotmail.com>
2022-05-04 13:21:16 +0300

    add 08 Simple Pig Latin

---
+++ 08 Simple Pig Latin/Description.md
@@ -0,0 +1,7 @@
+﻿Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+## Examples
+```csharp
+Kata.PigIt("Pig latin is cool"); // igPay atinlay siay oolcay
+Kata.PigIt("Hello world !");     // elloHay orldway !
+```
---
+++ 08 Simple Pig Latin/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static string PigIt(string str)
+    {
+        var result = new List<string>();
+        var parts = str.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var part in parts)
+        {
+            var firstLetter = part[0];
+            if (char.IsLetter(firstLetter))
+            {
+                string item = $"{part.Remove(0, 1)}{firstLetter}ay";
+                result.Add(item);
+            }
+            else
+            {
+                result.Add(part);
+            }
+        }
+        return String.Join(" ", result);  }
+}
