7dbb502391ad6a3720c82dee2de763a823b82f9f
Максим Чижиков <chizhikov.max@hotmail.com>
2022-05-04 13:30:55 +0300

    add 09 Split Strings

---
+++ 09 Split Strings/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public class SplitString
+{
+    public static string[] Solution(string str)
+    {
+        if (str.Length % 2 == 1){
+            str+="_";
+        }
+        var result = new List<string>();
+        for (int i=0; i< str.Length; i+=2) {
+            result.Add(str.Substring(i,2));
+        }
+        return result.ToArray();
+    }
+}
