12745cfb5757d7f50f24e88d9f415aea2e64f544
Максим Чижиков <chizhikov.max@hotmail.com>
2022-05-04 13:15:41 +0300

    add 07 Where my anagrams at

---
+++ 07 Where my anagrams at/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+    public static List<string> Anagrams(string word, List<string> words)
+    {
+        bool IsAnagram(string x, string y)
+        {
+            var stringX = new string(x.OrderBy(z => z).ToArray());
+            var stringY = new string(y.OrderBy(z => z).ToArray());
+            return stringX == stringY;
+        }
+
+        var result = new List<string>();
+        foreach (var item in words)
+        {
+            if (IsAnagram(word, item))
+                result.Add(item);
+        }
+        return result;
+    }
+}
